ed6e881a0a60f88c0aa4949577c984ce895f5003
Filip <fi.witkowski@gmail.com>
2020-02-05 15:38:33 +0100

    Merge branch 'Fifi' of https://github.com/FiWitkowski/MyApp into Fifi
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/myapp/Users/User.java
+++ app/src/main/java/com/example/myapp/Users/User.java
@@ -1,28 +1,23 @@
 package com.example.myapp.Users;
 
-import android.provider.ContactsContract;
-
-import java.util.Date;
 
 public abstract class User {
     protected String userName;
     protected String password;
     public String firstName, lastName;
     public int age;
-    public Date dateOfBirth;
     public String emailAddress;
     public AccessLevel typeOfUser;
     public User(){
 
     }
 
-    public User(String userName, String password, String firstName, String lastName, int age, Date dateOfBirth, String emailAddress, AccessLevel typeOfUser) {
+    public User(String userName, String password, String firstName, String lastName, int age, String emailAddress, AccessLevel typeOfUser) {
         this.userName = userName;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.dateOfBirth = dateOfBirth;
         this.emailAddress = emailAddress;
         this.typeOfUser = typeOfUser;
     }
