2815444d03a8685cc2f98cdb2dbce271e84117b0
Ola <alessandra.kralka@gmail.com>
2020-02-05 17:04:34 +0100

    everything is added :D

---
+++ app/src/main/java/com/example/myapp/TeacherActivity/StudentList.java
@@ -0,0 +1,68 @@
+package com.example.myapp.TeacherActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.myapp.LayoutHandle.MainController;
+import com.example.myapp.Misc.Subject;
+import com.example.myapp.R;
+import com.example.myapp.Users.Student;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StudentList extends AppCompatActivity {
+
+    private ListView list ;
+
+    private MainController controller=MainController.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_list);
+        list = (ListView) findViewById(R.id.listview);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        final Class schoolClass = (Class) extras.getSerializable("SCHOOL_CLASS");
+        final Subject subject = (Subject) extras.getSerializable("SUBJECT");
+        String[] from = new String[]{"rowid", "col_1", "col_2"};
+        int[] to = new int[]{R.id.item1, R.id.item2, R.id.item3};
+        List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
+        final List<Student> students = MainController.getInstance().getListOfStudents(schoolClass);
+
+        for (int i = 0; i < students.size(); i++) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("rowid", "" + (i + 1));
+            map.put("col_1", students.get(i).getLastName());
+            map.put("col_2", students.get(i).getFirstName());
+            fillMaps.add(map);
+        }
+        SimpleAdapter adapter = new SimpleAdapter(this, fillMaps, R.layout.simple_list_item_1, from, to);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position, long l){
+                Student itempupil = (Student) adapter.getItemAtPosition(position);
+                final Bundle bundle = new Bundle();
+                bundle.putSerializable("CHILD", (Serializable) students.get(position));
+                bundle.putSerializable("SUBJECT",subject);
+                bundle.putSerializable("SCHOOL_CLASS", schoolClass);
+
+                Intent intent = new Intent(StudentList.this, GradeList.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/myapp/Users/Teacher.java
+++ app/src/main/java/com/example/myapp/Users/Teacher.java
@@ -1,7 +1,47 @@
 package com.example.myapp.Users;
 
+import com.example.myapp.LayoutHandle.Register;
+
+import java.util.List;
+
 public class Teacher extends User {
-   // public Teacher(String firstName, String lastName, String userName, String password) {
-     //   super(firstName,lastName,userName,password);
-    //}
+    private List<Student> children;
+    public Teacher(String userName, String password, String firstName, String lastName, int age, String emailAddress, AccessLevel typeOfUser){
+        super(userName, password, firstName, lastName, age, emailAddress, typeOfUser);
+    }
+    public List<Student> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Student> children) {
+        this.children = children;
+    }
+
+    public String getEmail() {
+        return emailAddress;
+    }
+
+    public void setEmail(String email) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
